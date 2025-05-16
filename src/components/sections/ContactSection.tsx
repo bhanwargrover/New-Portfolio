7a00833f50c53fd1f402cc6443a11b242f32efd4
Bhanwar Grover <bhavargrover4@gmail.com>
2025-05-16 17:23:22 +0530

    Update ContactSection.tsx

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -23,7 +23,7 @@ const ContactSection: React.FC = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://new-portfolio-ep71.onrender.com/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
